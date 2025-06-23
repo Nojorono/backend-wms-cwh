c1550a847d2cf931d25337e6f4beb2ecfa242e48
bagasargita <argitabagasmahasmara@gmail.com>
2025-06-23 11:07:52 +0700

    #fix migration

--- src/core/domain/entities/user.entity.ts
+++ src/core/domain/entities/user.entity.ts
@@ -20,6 +20,9 @@ export class User {
   @Column({ unique: true, length: 100 })
   username: string;
 
+  @Column({ name: 'organization_id', nullable: true })
+  organizationId: number;
+
   @Column({ length: 255 })
   password: string;
 
