d78944a635a10d149a65be09ab642a7f1401038b
bagasargita <argitabagasmahasmara@gmail.com>
2025-06-23 09:22:44 +0700

    #fix migration

--- src/infrastructure/database/migrations/1750314715223-MasterSupplier.ts
+++ src/infrastructure/database/migrations/1750314715223-MasterSupplier.ts
@@ -4,7 +4,7 @@ export class MasterSupplier1750314715223 implements MigrationInterface {
     name = 'MasterSupplier1750314715223'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "m_supplier" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "organization_id" integer, "operating_unit" character varying, "supplier_code" character varying, "supplier_name" character varying, "supplier_address" character varying, "supplier_phone" character varying, "supplier_email" character varying, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_m_supplier" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "m_supplier" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "organization_id" integer, "operating_unit" character varying, "supplier_code" character varying, "supplier_name" character varying, "supplier_address" character varying, "supplier_contact_person" character varying, "supplier_phone" character varying, "supplier_email" character varying, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_m_supplier" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "m_pallet" DROP COLUMN "client_id"`);
         await queryRunner.query(`ALTER TABLE "m_pallet" DROP CONSTRAINT "UQ_m_pallet_code"`);
         await queryRunner.query(`ALTER TABLE "m_pallet" DROP COLUMN "code"`);
