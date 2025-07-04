dc05695ea49e17f53eaf4426ec311e65a29e73c5
bagasargita <argitabagasmahasmara@gmail.com>
2025-07-04 13:55:25 +0700

    fix: many m_warehouse

--- src/master-warehouse/master-warehouse.service.ts
+++ src/master-warehouse/master-warehouse.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, ConflictException, BadRequestException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { MasterWarehouseRepository } from './master-warehouse.repository';
 import { CreateMasterWarehouseDto } from './dto/create-master-warehouse.dto';
 import { UpdateMasterWarehouseDto } from './dto/update-master-warehouse.dto';
@@ -9,14 +9,6 @@ export class MasterWarehouseService {
   constructor(private readonly repository: MasterWarehouseRepository) {}
 
   async create(createMasterWarehouseDto: CreateMasterWarehouseDto): Promise<MasterWarehouse> {
-    const organizationId = createMasterWarehouseDto.organization_id;
-    if (!organizationId) {
-      throw new BadRequestException('Organization ID is required');
-    }
-    const existingWarehouse = await this.repository.findByOrganizationId(organizationId);
-    if (existingWarehouse) {
-      throw new ConflictException(`Warehouse with organization ID ${organizationId} already exists`);
-    }
     return await this.repository.create(createMasterWarehouseDto);
   }
 
@@ -33,13 +25,6 @@ export class MasterWarehouseService {
   }
 
   async update(id: string, updateMasterWarehouseDto: UpdateMasterWarehouseDto): Promise<MasterWarehouse> {
-    const warehouse = await this.findOne(id);
-    if (updateMasterWarehouseDto.organization_id && updateMasterWarehouseDto.organization_id !== warehouse.organization_id) {
-      const existingWarehouse = await this.repository.findByOrganizationId(updateMasterWarehouseDto.organization_id);
-      if (existingWarehouse) {
-        throw new ConflictException(`Warehouse with organization ID ${updateMasterWarehouseDto.organization_id} already exists`);
-      }
-    }
     const updatedWarehouse = await this.repository.update(id, updateMasterWarehouseDto);
     if (!updatedWarehouse) {
       throw new NotFoundException(`Warehouse with ID ${id} not found`);
