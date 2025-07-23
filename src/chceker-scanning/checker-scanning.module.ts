93c429b2dee5ec47f630763f553274072adc3f5b
bagasargita <argitabagasmahasmara@gmail.com>
2025-07-23 16:52:15 +0700

    scan create

--- src/chceker-scanning/checker-scanning.module.ts
+++ src/chceker-scanning/checker-scanning.module.ts
@@ -7,9 +7,11 @@ import { CheckerScanningRepository } from './checker-scanning.repository';
 import { User } from 'src/core/domain/entities/user.entity';
 import { InboundDeliveryOrder } from 'src/core/domain/entities/inbound-delivery-order.entity';
 import { InboundPlan } from 'src/core/domain/entities/inbound-plan.entity';
+import { MasterItem } from 'src/core/domain/entities/master-item.entity';
+import { InboundDeliveryOrderItem } from 'src/core/domain/entities/inbound-delivery-order-item.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CheckerScanning, User, InboundDeliveryOrder, InboundPlan])],
+  imports: [TypeOrmModule.forFeature([CheckerScanning, User, InboundDeliveryOrder, InboundPlan, MasterItem, InboundDeliveryOrderItem])],
   controllers: [CheckerScanningController],
   providers: [
     CheckerScanningService,
