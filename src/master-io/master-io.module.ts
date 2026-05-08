b342f362ffe7c1a3f9fe7803bf749f06ce637f4e
bagasargita <argitabagasmahasmara@gmail.com>
2026-05-08 10:11:14 +0700

    fix: master io sync ebs oralce

--- src/master-io/master-io.module.ts
+++ src/master-io/master-io.module.ts
@@ -4,11 +4,33 @@ import { MasterIO } from '../core/domain/entities/master-io.entity';
 import { MasterIOController } from './master-io.controller';
 import { MasterIOService } from './master-io.service';
 import { MasterIORepository } from './master-io.repository';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { IOIntegrationService } from './integration/io-integration.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MasterIO])],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([MasterIO]),
+    ClientsModule.registerAsync([
+      {
+        name: 'ONT_BRANCHES_SERVICE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get('RABBITMQ_URL', 'amqp://localhost:5672') as string],
+            queue: configService.get('rmq.ontBranches') || 'ont_branches_queue',
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   controllers: [MasterIOController],
-  providers: [MasterIOService, MasterIORepository],
+  providers: [MasterIOService, MasterIORepository, IOIntegrationService],
   exports: [MasterIOService],
 })
 export class MasterIOModule {}
